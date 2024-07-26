15e8ea649aaf90aeb5c971796ac1c4a04307ded0
chavvaruchitha123 <150325332+chavvaruchitha123@users.noreply.github.com>
2024-07-26 19:20:42 +0530

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -11,77 +11,145 @@
 //             __/ |
 //            |___/
 
+// module.exports = {
+//   // An array of folders (excluding subfolders) where your tests are located;
+//   // if this is not specified, the test source must be passed as the second argument to the test runner.
+//   src_folders: ['test','nightwatch/examples'],
+
+//   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+//   page_objects_path: ['nightwatch/page-objects'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
+//   custom_commands_path: ['nightwatch/custom-commands'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
+//   custom_assertions_path: ['nightwatch/custom-assertions'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
+//   plugins: [],
+  
+//   // See https://nightwatchjs.org/guide/concepts/test-globals.html
+//   globals_path: '',
+  
+//   webdriver: {},
+
+//   test_workers: {
+//     enabled: true
+//   },
+
+//   test_settings: {
+//     default: {
+//       disable_error_log: false,
+//       launch_url: 'http://localhost',
+
+//       screenshots: {
+//         enabled: false,
+//         path: 'screens',
+//         on_failure: true
+//       },
+
+//       desiredCapabilities: {
+//         browserName: 'chrome'
+//       },
+      
+//       webdriver: {
+//         start_process: true,
+//         server_path: ''
+//       },
+      
+//     },
+    
+//     chrome: {
+//       desiredCapabilities: {
+//         browserName: 'chrome',
+//         'goog:chromeOptions': {
+//           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+//           args: [
+//             //'--no-sandbox',
+//             //'--ignore-certificate-errors',
+//             //'--allow-insecure-localhost',
+//             //'--headless=new'
+//           ]
+//         }
+//       },
+
+//       webdriver: {
+//         start_process: true,
+//         server_path: '',
+//         cli_args: [
+//           // --verbose
+//         ]
+//       }
+//     },
+    
+//   },
+  
+// };
+const chromedriver = require('chromedriver');
+ 
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['test','nightwatch/examples'],
-
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+  src_folders: ['test', 'nightwatch/examples'],
   page_objects_path: ['nightwatch/page-objects'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   custom_commands_path: ['nightwatch/custom-commands'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
   custom_assertions_path: ['nightwatch/custom-assertions'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: '',
-  
-  webdriver: {},
-
+  webdriver: {
+    start_process: true,
+    server_path: chromedriver.path,
+    port: 2034,
+    cli_args: [
+      '--verbose'
+    ]
+  },
+ 
   test_workers: {
     enabled: true
   },
-
   test_settings: {
     default: {
       disable_error_log: false,
-      launch_url: 'http://localhost',
-
       screenshots: {
         enabled: false,
         path: 'screens',
         on_failure: true
       },
-
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: [
+            '--headless',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--window-size=1920,1080'
+          ]
+        }
       },
-      
-      webdriver: {
-        start_process: true,
-        server_path: ''
-      },
-      
+      globals: {
+        devServerURL: 'http://localhost:2034' // Your custom URL
+      }
     },
-    
     chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
-          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           args: [
-            //'--no-sandbox',
-            //'--ignore-certificate-errors',
-            //'--allow-insecure-localhost',
-            //'--headless=new'
+            '--headless',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--window-size=1920,1080'
           ]
         }
       },
-
       webdriver: {
         start_process: true,
-        server_path: '',
+        server_path: chromedriver.path,
         cli_args: [
-          // --verbose
+          '--verbose'
         ]
       }
-    },
-    
-  },
-  
+    }
+  }
 };
