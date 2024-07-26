7382ca76ea8d5bfa68955dec6bef205f00c4754b
chavvaruchitha123 <150325332+chavvaruchitha123@users.noreply.github.com>
2024-07-26 19:31:54 +0530

    Update night.js

--- test/night.js
+++ test/night.js
@@ -2,15 +2,15 @@ module.exports = {
   'Google advanced search: nightwatch': function (browser) {
     browser
       .url('http://localhost:5174/')
-      .waitForElementPresent('input[id="first name"]', 10000)
+      .waitForElementPresent('input[id="first name"]', 15000)
       .setValue('input[id="first name"]', "deepthi")
-      .waitForElementPresent('input[id="second name"]', 10000)
+      .waitForElementPresent('input[id="second name"]', 15000)
       .setValue('input[id="second name"]', "k")
-      .waitForElementPresent('input[id="email"]', 10000)
+      .waitForElementPresent('input[id="email"]', 15000)
       .setValue('input[id="email"]', "abhi@gmail.com")
-      .waitForElementPresent('input[type="password"]', 10000)
+      .waitForElementPresent('input[type="password"]', 15000)
       .setValue('input[type="password"]', 'Pass@123')
-      .waitForElementPresent('button[id="submit"]', 10000)
+      .waitForElementPresent('button[id="submit"]', 15000)
       .click('button[id="submit"]')
       .saveScreenshot("tests_output/google1.png")
       .end();
